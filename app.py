3242ef2d1c3e2e5ffc00e3a1ad79a91014da22ae
Ananth Kamath <infinitekamath@gmail.com>
2024-05-23 13:13:34 +0530

    Add requests dependency and code which uses it

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, json
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Flask says Hello!!!</p>"
+
+@app.route("/ditto")
+def ditto():
+    res = requests.get("https://pokeapi.co/api/v2/pokemon/ditto")
+    return res.text
